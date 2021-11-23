916db941d9ffbcee9076c171316c6e5797c22637
leedy <yulidaiahx@163.com>
2021-11-23 20:21:54 +0800

    impl Add/Sub/Mul for FieldElement

--- src/field.rs
+++ src/field.rs
@@ -1,4 +1,6 @@
 use std::cmp::PartialEq;
+use std::ops::{Add, Sub, Mul};
+use num::checked_pow;
 
 pub struct FieldElement {
     num: i32,
@@ -25,6 +27,48 @@ impl PartialEq for FieldElement {
     }
 }
 
+impl Add for FieldElement {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        if self.prime != other.prime {
+            panic!("cannot add two numbers in different Fields");
+        }
+        Self {
+            num: (self.num + other.num) % self.prime,
+            prime: self.prime
+        }
+    }
+}
+
+impl Sub for FieldElement {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        if self.prime != other.prime {
+            panic!("cannot add two numbers in different Fields");
+        }
+        Self {
+            num: (self.num - other.num) % self.prime,
+            prime: self.prime
+        }
+    }
+}
+
+impl Mul for FieldElement {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        if self.prime != other.prime {
+            panic!("cannot add two numbers in different Fields");
+        }
+        Self {
+            num: (self.num * other.num) % self.prime,
+            prime: self.prime
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     pub use super::FieldElement;
@@ -48,15 +92,69 @@ mod tests {
 
     #[test]
     fn eq_success() {
-        let field1 = FieldElement::new(5, 11).unwrap();
-        let field2 = FieldElement::new(5, 11).unwrap();
-        assert!(field1 == field2);
+        let element1 = FieldElement::new(5, 11).unwrap();
+        let element2 = FieldElement::new(5, 11).unwrap();
+        assert!(element1 == element2);
     }
 
     #[test]
     fn ne_success() {
-        let field1 = FieldElement::new(5, 11).unwrap();
-        let field2 = FieldElement::new(8, 11).unwrap();
-        assert!(field1 != field2);
+        let element1 = FieldElement::new(5, 11).unwrap();
+        let element2 = FieldElement::new(8, 11).unwrap();
+        assert!(element1 != element2);
+    }
+
+    #[test]
+    fn add_success() {
+        let element1 = FieldElement::new(7, 13).unwrap();
+        let element2 = FieldElement::new(12, 13).unwrap();
+
+        let element3 = FieldElement::new(6, 13).unwrap();
+
+        assert!(element1 + element2 == element3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_failed() {
+        let element1 = FieldElement::new(7, 11).unwrap();
+        let element2 = FieldElement::new(12, 13).unwrap();
+        let _ = element1 + element2;
+    }
+
+    #[test]
+    fn sub_success() {
+        let element1 = FieldElement::new(7, 13).unwrap();
+        let element2 = FieldElement::new(12, 13).unwrap();
+
+        let element3 = FieldElement::new(5, 13).unwrap();
+
+        assert!(element2 - element1 == element3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn sub_failed() {
+        let element1 = FieldElement::new(7, 11).unwrap();
+        let element2 = FieldElement::new(12, 13).unwrap();
+        let _ = element2 - element1;
+    }
+
+    #[test]
+    fn mul_success() {
+        let element1 = FieldElement::new(3, 13).unwrap();
+        let element2 = FieldElement::new(12, 13).unwrap();
+
+        let element3 = FieldElement::new(10, 13).unwrap();
+
+        assert!(element2 * element1 == element3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn mul_failed() {
+        let element1 = FieldElement::new(3, 11).unwrap();
+        let element2 = FieldElement::new(12, 13).unwrap();
+        let _ = element1 * element2;
     }
 }

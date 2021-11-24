a5aa6da21528aa6f0dd55736f4d43131c9355303
leedy <yulidaiahx@163.com>
2021-11-24 21:37:48 +0800

    divide Point into EccPoint and Point

--- src/point.rs
+++ src/point.rs
@@ -1,33 +1,49 @@
+#[derive(Clone)]
 pub struct Point {
-    x: i32,
-    y: i32,
+    pub x: i32,
+    pub y: i32,
+}
+
+pub struct EccPoint {
+    point: Option<Point>,
     a: i32,
     b: i32,
 }
 
-impl Point {
-    pub fn new(x: i32, y: i32, a: i32, b: i32) -> Result<Self, String> {
-        // y^2 = x^3 + ax + b
-        if y.pow(2) != x.pow(3) + a*x + b {
-            let msg = format!("({}, {}) is not on the curve", x, y);
-            return Err(msg);
+impl EccPoint {
+    pub fn new(point: Option<Point>, a: i32, b: i32) -> Result<Self, String> {
+        if let Some(point_real) = point.clone() {
+            // y^2 = x^3 + ax + b
+            if point_real.y.pow(2) != point_real.x.pow(3) + a*point_real.x + b {
+                let msg = format!("({}, {}) is not on the curve", point_real.x, point_real.y);
+                return Err(msg);
+            }
         }
-        Ok(Self { x, y, a, b })
+
+        // EccPoint is infinity if Point is none
+        Ok(Self { point, a, b })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Point;
+    use super::{Point, EccPoint};
 
     #[test]
-    fn create_point_success() {
-        Point::new(-1, -1, 5, 7).unwrap();
+    fn create_ecc_point_with_none() {
+        EccPoint::new(None, 5, 7).unwrap();
+    }
+
+    #[test]
+    fn create_ecc_point_with_point() {
+        let point = Point { x: -1, y: -1 };
+        EccPoint::new(Some(point), 5, 7).unwrap();
     }
 
     #[test]
     #[should_panic]
-    fn create_point_fail() {
-        Point::new(-1, -2, 5, 7).unwrap();
+    fn create_ecc_point_fail() {
+        let point = Point { x: -1, y: -2 };
+        EccPoint::new(Some(point), 5, 7).unwrap();
     }
 }

eded3bbab14bf76e51b03b0ba9c80e6674dca336
leedy <yulidaiahx@163.com>
2021-12-14 19:31:42 +0800

    return used by Script::parse()

--- src/script/script.rs
+++ src/script/script.rs
@@ -25,19 +25,24 @@ impl Script {
         Self { cmds }
     }
 
-    pub fn parse(bytes: &[u8]) -> Result<Self, Error> {
+    pub fn parse(bytes: &[u8]) -> Result<(Self, usize), Error> {
         let (length, used) = varint::decode(bytes)?;
-        let total = length + used as u64;
-        if total != bytes.len() as u64 {
+        let total = length + used as usize;
+        if total > bytes.len() {
             return Err(Error::InvalidBytes);
         }
 
         let used = used as usize;
-        Self::parse_raw(&bytes[used..])
+        let (script, used_real) = Self::parse_raw(&bytes[used..total])?;
+        if length != used_real {
+            return Err(Error::InvalidBytes);
+        }
+
+        Ok((script, total))
     }
 
     // without len prefix
-    pub fn parse_raw(bytes: &[u8]) -> Result<Self, Error> {
+    pub fn parse_raw(bytes: &[u8]) -> Result<(Self, usize), Error> {
         let mut cmds = Vec::new();
 
         let mut index = 0;
@@ -49,7 +54,7 @@ impl Script {
             cmds.push(element);
         }
 
-        Ok(Self { cmds })
+        Ok((Self { cmds }, index))
     }
 
     pub fn serialize(&self) -> Result<Vec<u8>, Error> {
@@ -125,10 +130,12 @@ mod tests {
     #[test]
     fn script_evaluate_p2pkh_for_uncompressed_pk() {
         let script_pubkey = hex::decode("76a914fb6c931433c83e8bb5a4c6588c7fc24c08dac6e388ac").unwrap();
-        let script_pubkey = Script::parse_raw(&script_pubkey).unwrap();
+        let (script_pubkey, script_pubkey_used) = Script::parse_raw(&script_pubkey).unwrap();
+        assert_eq!(script_pubkey_used, 25);
 
         let script_sig = hex::decode("473045022000eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c022100c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab64104887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34").unwrap();
-        let script_sig = Script::parse_raw(&script_sig).unwrap();
+        let (script_sig, script_sig_used) = Script::parse_raw(&script_sig).unwrap();
+        assert_eq!(script_sig_used, 138);
 
         let combined_script = script_sig + script_pubkey;
         let z = U256::from_big_endian(&hex::decode("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d").unwrap());
@@ -139,10 +146,12 @@ mod tests {
     #[test]
     fn script_evaluate_add_euqal() {
         let script_pubkey = hex::decode("55935987").unwrap();
-        let script_pubkey = Script::parse_raw(&script_pubkey).unwrap();
+        let (script_pubkey, script_pubkey_used) = Script::parse_raw(&script_pubkey).unwrap();
+        assert_eq!(script_pubkey_used, 4);
 
         let script_sig = hex::decode("54").unwrap();
-        let script_sig = Script::parse_raw(&script_sig).unwrap();
+        let (script_sig, script_sig_used) = Script::parse_raw(&script_sig).unwrap();
+        assert_eq!(script_sig_used, 1);
 
         let combined_script = script_sig + script_pubkey;
         let result = combined_script.evaluate(U256::zero()).unwrap();
